27a185f15dd8436ed4e73b82c989dfa4c0577387
Toni ALMAGRO <Antonio.ALMAGRO@amadeus.com>
2020-11-19 21:32:46 +0100

    test_hacker now with loop on listFiles

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 #
-#
+# Creating temp folder
 folder="ref_repo"
 mkdir $folder
+
+# Cloning repo into folder
 git clone https://github.com/rgomezh/campechano.git $folder
+listFiles=$(find $folder -name '*.out')
+echo $listFiles
+
+for file in $listFiles; do
+echo $file
+done
+
+#Deleting folder
 rm -rf $folder
 echo "repo cloned, removing folder $folder"
