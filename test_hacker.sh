7447783b6515c733bf9a630a811c0e131a934f1e
Toni ALMAGRO <Antonio.ALMAGRO@amadeus.com>
2020-11-19 21:41:47 +0100

    adding dummies out files

--- test_hacker.sh
+++ test_hacker.sh
@@ -2,17 +2,25 @@
 #
 # Creating temp folder
 folder="ref_repo"
+diff_folder="diff"
 mkdir $folder
+mkdir $diff_folder
 
 # Cloning repo into folder
 git clone https://github.com/rgomezh/campechano.git $folder
-listFiles=$(find $folder -name '*.out')
-echo $listFiles
+list_origin=$(find . -name '*.out')
+list_ref=$(find $folder -name '*.out')
 
-for file in $listFiles; do
-echo $file
+i=0
+for file in $list_ref; do
+    echo $i $file
+    file0=${list_origin[$i]}
+    diff $file0 file > $diff_folder/$file0.diff
+    i=$((i+1))
+    
 done
 
 #Deleting folder
 rm -rf $folder
+##rm -rf $diff_folder
 echo "repo cloned, removing folder $folder"
